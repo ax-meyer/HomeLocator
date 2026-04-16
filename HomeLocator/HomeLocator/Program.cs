78059ddd0f34b94ad49915bd83d0abe0155d012d
ax-meyer <32029723+ax-meyer@users.noreply.github.com>
2026-04-16 20:47:45 +0200

    stuff

--- HomeLocator/HomeLocator/Program.cs
+++ HomeLocator/HomeLocator/Program.cs
@@ -89,7 +89,6 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 app.UseRateLimiter();
 app.UseAntiforgery();
 
